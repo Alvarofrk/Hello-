a55d506b11310b0eaf585178416ed1ff299da564
jamil29 <33689725+jamil29@users.noreply.github.com>
2018-03-20 09:14:51 -0500

    Update main.cpp

--- hola/main.cpp
+++ hola/main.cpp
@@ -5,5 +5,8 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
+    cout << "alvaro cerna ramos" << endl;
+    cout << " freddy jamil hurtado" << endl;
+        
     return 0;
 }
